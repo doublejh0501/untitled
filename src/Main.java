d18e20a7ce6d6aa192af9e97d69e4702f49f8724
a <a@as-MacBook-Pro.local>
2025-06-17 17:34:14 +0900

    별찍기 1-10

---
+++ src/Main.java
@@ -0,0 +1,84 @@
+public class Main {
+
+    public static void problem1(int n) {
+       for(int i =0; i <=n; i++) {
+           for(int j=0; j <=n; j++) {
+               System.out.print("*");
+           }
+           System.out.println();
+       }
+    }
+    public static void problem2(int n) {
+        for(int i=0; i <=n; i++) {
+            for(int j =0; j <=i; j++) {
+                if (i ==j) {
+                    System.out.print("*");
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void problem3(int n) {
+        for(int i =0; i<=n; i++) {
+            for(int j=0; j <= n; j++) {
+                System.out.print(" ");
+                if(n-i == j) {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void problem4(int n) {
+        for(int i=0; i <=n; i++) {
+            for(int j=0; j <= n; j++) {
+                if(j == i || j==n-i) {
+                    System.out.print("*");
+                }
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void problem5(int n) {
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void problem6(int n) {
+        for(int i =0; i<=n; i++) {
+            for(int j=0; j <=i; j++) {
+                if(j >= n-i) {
+                    System.out.print("*");
+                }
+                else
+                    System.out.print(" ");
+
+            }
+            System.out.println();
+        }
+
+    }
+    public static void problem7(int n) {
+
+    }
+    public static void problem8(int n) {
+
+    }
+    public static void main(String[] args) {
+        problem1(4);
+        problem2(4);
+        problem3(4);
+        problem4(4);
+        problem5(4);
+        problem6(4);
+        problem7(4);
+        problem8(4);
+
+    }
+}
