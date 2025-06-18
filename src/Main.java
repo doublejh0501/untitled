7de7e73b2384039f53438f2b854a2522da90a6dc
Jaehun <jaehun1417@gmail.com>
2025-06-18 11:50:58 +0900

    별찍기 1-10

--- src/Main.java
+++ src/Main.java
@@ -52,7 +52,7 @@ public class Main {
     }
     public static void problem6(int n) {
         for(int i =0; i<=n; i++) {
-            for(int j=0; j <=i; j++) {
+            for(int j=0; j <=n; j++) {
                 if(j >= n-i) {
                     System.out.print("*");
                 }
@@ -62,12 +62,67 @@ public class Main {
             }
             System.out.println();
         }
-
     }
     public static void problem7(int n) {
-
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= i+5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
     public static void problem8(int n) {
+        for(int i =0; i <=n; i++) {
+            for(int j=0; j <= n*2; j++) {
+                if(n+i >= j && n-i <=j) {
+                    System.out.print("*");
+                }
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void problem9(int n) {
+        for(int i =0; i<=n; i++) {
+            for(int j = 0; j<=n; j++) {
+                if(i <= n/2) {
+                    if (i <= j && n - i >= j)
+                        System.out.print("*");
+                    else
+                        System.out.print(" ");
+                }
+                else if(i > n/2) {
+                    if(i >= j && n-i <=j) {
+                        System.out.print("*");
+                    }
+                    else
+                        System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void problem10(int n) {
+        for(int i =0; i <=n; i++) {
+            for(int j =0; j <=n; j++) {
+                if(i <= n/2) {
+                    if (j <= i || j >= n-i )
+                        System.out.print("*");
+                    else
+                        System.out.print(" ");
+                }
+                else if(i > n/2) {
+                    if(j <= n-i || j >= i) {
+                        System.out.print("*");
+                    }
+                    else
+                        System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
 
     }
     public static void main(String[] args) {
@@ -79,6 +134,8 @@ public class Main {
         problem6(4);
         problem7(4);
         problem8(4);
+        problem9(4);
+        problem10(4);
 
     }
 }
